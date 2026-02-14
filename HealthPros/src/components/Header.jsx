7c0c7176c4e0dba114a2a7e4c813142a4bd89946
monaheng101 <ntaimonaheng@gmail.com>
2026-02-14 21:11:43 +0200

    header section

---
+++ HealthPros/src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        {/* ----------------------- Left Side ---------------------- */}
+        <div>
+
+        </div>
+
+        {/* ----------------------- Right Side ---------------------- */}
+        <div>
+            
+        </div>
+
+    </div>
+  )
+}
+
+export default Header
